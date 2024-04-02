97e7e59cf16116c81b8c8b7f90459968c85a5db7
Toreakhmet <s.vasilovich@bk.ru>
2024-04-02 22:23:39 +0500

    add Excursion model

--- main_site/admin.py
+++ main_site/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Excursion
 
+
+admin.site.register(Excursion)
 # Register your models here.
--- main_site/models.py
+++ main_site/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Excursion(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    lng = models.FloatField()
+    lat = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    excursion = models.ForeignKey(Excursion, related_name='images', on_delete=models.CASCADE)
+    url = models.URLField()
+
+    def __str__(self):
+        return f"Image for {self.excursion.title}"
